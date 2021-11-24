1cf006c30d7d695236a26243373378cf91a0d294
Jack Evans <jackevans24@googlemail.com>
2021-11-24 12:27:39 +0000

    Add delay to colour changer Add colour changer to item boxes

--- Assets/Scripts/BoostColour.cs
+++ Assets/Scripts/BoostColour.cs
@@ -7,10 +7,12 @@ public class BoostColour : MonoBehaviour
     [SerializeField, ColorUsage(showAlpha: true)] private Color boostColor;
     [SerializeField] private float fadeTime = 1f;
     [SerializeField] private Ease easing = Ease.InOutSine;
+    [SerializeField] private bool addDelay = false;
 
     private void Awake()
     {
         var mesh = GetComponent<MeshRenderer>();
-        mesh.material.DOColor(this.boostColor, this.fadeTime).SetLoops(-1, LoopType.Yoyo).SetEase(this.easing);
+        var delay = this.addDelay ? Random.Range(0, this.fadeTime) : 0;
+        mesh.material.DOColor(this.boostColor, this.fadeTime).SetDelay(delay).SetLoops(-1, LoopType.Yoyo).SetEase(this.easing);
     }
 }
