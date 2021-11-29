a75b6008386d55737ee4e68bfd19af7ba07964c1
Jack Evans <jackevans24@googlemail.com>
2021-11-29 15:59:57 +0000

    Add wiggle to fireflies

--- Assets/Scripts/HoldingBugController.cs
+++ Assets/Scripts/HoldingBugController.cs
@@ -1,7 +1,7 @@
 using DG.Tweening;
 using UnityEngine;
 
-public class HoldingBugController : MonoBehaviour
+public class HoldingBugController : WiggleBug
 {
     [Header("References")]
     [SerializeField] Transform player;
@@ -10,44 +10,12 @@ public class HoldingBugController : MonoBehaviour
     [Header("Positioning")]
     public Vector3 InitialOffset;
 
-    [Header("Wiggle")]
-    [SerializeField] private Vector3 wiggleDistance;
-    [SerializeField] private Vector2 wiggleSpeedBounds;
-    [SerializeField] private Ease wiggleEasing = Ease.InOutSine;
-
     [Header("Fly off")]
     [SerializeField] private Vector3 letGoDistance;
     [SerializeField] private float letGoDuration;
     [SerializeField] private Ease letGoEasing = Ease.InSine;
 
     private bool holding = true;
-    private Tween[] wiggleTweens = new Tween[0];
-
-    private void Start()
-    {
-        this.ResetWiggle();
-    }
-
-    public void ResetWiggle()
-    {
-        foreach (var tween in this.wiggleTweens)
-            tween.Kill();
-
-        var xTween = this.transform
-            .DOMoveX(this.transform.position.x + this.wiggleDistance.x, Random.Range(this.wiggleSpeedBounds.x, this.wiggleSpeedBounds.y))
-            .SetLoops(-1, LoopType.Yoyo)
-            .SetEase(this.wiggleEasing);
-        var yTween = this.transform
-            .DOMoveY(this.transform.position.y + this.wiggleDistance.y, Random.Range(this.wiggleSpeedBounds.x, this.wiggleSpeedBounds.y))
-            .SetLoops(-1, LoopType.Yoyo)
-            .SetEase(this.wiggleEasing);
-        var zTween = this.transform
-            .DOMoveZ(this.transform.position.z + this.wiggleDistance.z, Random.Range(this.wiggleSpeedBounds.x, this.wiggleSpeedBounds.y))
-            .SetLoops(-1, LoopType.Yoyo)
-            .SetEase(this.wiggleEasing);
-
-        this.wiggleTweens = new[] { xTween, yTween, zTween };
-    }
 
     private void Update()
     {
