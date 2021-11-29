9c28f48c42107a98d9e08a8bc2f1018dee77b64d
Jack Evans <jackevans24@googlemail.com>
2021-11-29 14:20:59 +0000

    Add oak highway - night

--- Assets/Scripts/Constants/Scenes.cs
+++ Assets/Scripts/Constants/Scenes.cs
@@ -5,12 +5,13 @@ public enum Scenes
     Menu,
     OakHighway,
     Extras,
-    BugTesting
+    BugTesting,
+    OakHighwayNight
 }
 
 public static class ScenesExtensions
 {
-    private static Scenes[] raceScenes = new[] { Scenes.OakHighway, Scenes.BugTesting };
+    private static Scenes[] raceScenes = new[] { Scenes.OakHighway, Scenes.BugTesting, Scenes.OakHighwayNight };
 
     public static bool IsRace(this Scenes scene) => raceScenes.Contains(scene);
 }
